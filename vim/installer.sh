0a7af6808c9ad93fb733ac98ddfb11a6b32f842b
Maciej Józefczyk <mjozefcz@redhat.com>
2019-08-06 12:28:18 +0200

    Add YCM installation

--- vim/installer.sh
+++ vim/installer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install vim git
+sudo apt-get update && sudo apt-get install vim git build-essential cmake python3-dev
 mkdir -p ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/src/dotfiles/vim/vimrc ~/.vimrc
+vim +PluginInstall +qall
+
+# YCM compliation
+cd ~/.vim/bundle/YouCompleteMe/
+./install.py --clang-completer
+cd
 vim +PluginInstall +qall
